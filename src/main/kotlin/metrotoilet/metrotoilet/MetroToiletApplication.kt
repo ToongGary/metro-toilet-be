f72b987e30923089d6ee8ac696e0153288f0843c
LeeShinYeoung <iqrash12@gmail.com>
2022-12-29 01:10:45 +0900

    Initial commit

---
+++ src/main/kotlin/metrotoilet/metrotoilet/MetroToiletApplication.kt
@@ -0,0 +1,11 @@
+package metrotoilet.metrotoilet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MetroToiletApplication
+
+fun main(args: Array<String>) {
+	runApplication<MetroToiletApplication>(*args)
+}
---
+++ src/test/kotlin/metrotoilet/metrotoilet/MetroToiletApplicationTests.kt
@@ -0,0 +1,13 @@
+package metrotoilet.metrotoilet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MetroToiletApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
