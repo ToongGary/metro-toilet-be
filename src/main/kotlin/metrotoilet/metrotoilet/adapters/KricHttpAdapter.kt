205395acb8fe179ca64b4e26ba7a4bc8a6064d88
LeeShinYeoung <iqrash12@gmail.com>
2022-12-31 02:45:58 +0900

    Implement kric http adapter and dto

---
+++ src/main/kotlin/metrotoilet/metrotoilet/adapters/KricHttpAdapter.kt
@@ -0,0 +1,65 @@
+package metrotoilet.metrotoilet.adapters
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.core.type.TypeReference
+import metrotoilet.metrotoilet.adapters.dtos.*
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.*
+import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.client.RestTemplate
+
+@Component
+class KricHttpAdapter(
+    private val jacksonObjectMapper: ObjectMapper,
+    @Value("\${api.kric.station.endpoint}") val stationApiEndpoint: String,
+    @Value("\${api.kric.station.key}") val stationApiKey: String,
+    @Value("\${api.kric.stationToilet.endpoint") val stationToiletApiEndpoint: String,
+    @Value("\${api.kric.stationToilet.key") val stationToiletApiKey: String
+) {
+    fun requestStation(
+        stationRequestDto: StationRequestDto = StationRequestDto("", "")
+    ): StationResponseDto? {
+        val url = getRequestUrl(
+            stationApiEndpoint,
+            stationApiKey,
+            stationRequestDto
+        )
+        println(url)
+        return request<StationResponseDto>(url)
+    }
+
+    fun requestStationToilet(
+        stationToiletRequestDto: StationToiletRequestDto = StationToiletRequestDto("", "", "")
+    ): StationToiletResponseDto? {
+        val url = getRequestUrl(
+            stationToiletApiEndpoint,
+            stationToiletApiKey,
+            stationToiletRequestDto
+        )
+        return request<StationToiletResponseDto>(url)
+    }
+
+    private fun getRequestUrl(endpoint: String, key: String, dto: Any): String { // LinkedMultiValueMap<String, String> {
+        val params = LinkedMultiValueMap<String, String>()
+        val map = jacksonObjectMapper.convertValue(dto, object : TypeReference<Map<String, String>>() {})
+        params.setAll(map)
+
+        val queryString = StringBuilder()
+        queryString.append("serviceKey=$key&")
+        queryString.append("format=json&")
+
+        for ((key, value) in map.entries) {
+            queryString.append("$key=$value&")
+        }
+
+        return "$endpoint?$queryString"
+    }
+
+    private inline fun <reified T> request(url: String): T {
+        val restTemplate = RestTemplate()
+        val response = restTemplate.getForEntity(url, String::class.java)
+        val responseBody = response.body!!
+        return jacksonObjectMapper.readValue(responseBody, T::class.java)
+    }
+}
---
+++ src/main/kotlin/metrotoilet/metrotoilet/adapters/dtos/KricHttpAdapterDto.kt
@@ -0,0 +1,55 @@
+package metrotoilet.metrotoilet.adapters.dtos
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class StationRequestDto(
+    val mreaWideCd: String?,
+    val lnCd: String?
+)
+
+data class StationResponseDto(
+    @JsonProperty("header") val header: Header,
+    @JsonProperty("body") val body: List<Station>
+)
+
+data class StationToiletRequestDto(
+    val lnCd: String?,
+    val stinCd: String?,
+    val railOprIsttCd: String?
+)
+
+data class StationToiletResponseDto(
+    @JsonProperty("header") val header: Header,
+    @JsonProperty("body") val body: List<StationToilet>
+)
+
+data class Header(
+    @JsonProperty("resultCnt") val resultCnt: Int,
+    @JsonProperty("resultCode") val resultCode: String,
+    @JsonProperty("resultMsg") val resultMsg: String
+)
+
+data class Station(
+    @JsonProperty("mreaWideCd") val mreaWideCd: String,
+    @JsonProperty("routCd") val routCd: String,
+    @JsonProperty("routNm") val routNm: String,
+    @JsonProperty("stinConsOrdr") val stinConsOrdr: Int,
+    @JsonProperty("railOprIsttCd") val railOprIsttCd: String,
+    @JsonProperty("lnCd") val lnCd: String,
+    @JsonProperty("stinCd") val stinCd: String,
+    @JsonProperty("stinNm") val stinNm: String
+)
+
+data class StationToilet(
+    @JsonProperty("railOprIsttCd") val railOprIsttCd: String,
+    @JsonProperty("lnCd") val lnCd: String,
+    @JsonProperty("stinCd") val stinCd: String,
+    @JsonProperty("grndDvNm") val grndDvNm: String,
+    @JsonProperty("stinFlor") val stinFlor: Int,
+    @JsonProperty("gateInotDvNm") val gateInotDvNm: String,
+    @JsonProperty("exitNo") val exitNo: String,
+    @JsonProperty("dtlLoc") val dtlLoc: String,
+    @JsonProperty("mlFmlDvNm") val mlFmlDvNm: String,
+    @JsonProperty("toltNum") val toltNum: Int?,
+    @JsonProperty("diapExchNum") val diapExchNum: Int?
+)
