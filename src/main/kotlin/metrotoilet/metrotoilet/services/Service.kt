59d648b5816f6765389cae858bd00c587b0012b5
LeeShinYeoung <iqrash12@gmail.com>
2022-12-31 02:47:32 +0900

    Implement service layer

---
+++ src/main/kotlin/metrotoilet/metrotoilet/services/Service.kt
@@ -0,0 +1,35 @@
+package metrotoilet.metrotoilet.services
+
+import metrotoilet.metrotoilet.adapters.*
+import metrotoilet.metrotoilet.domains.*
+import metrotoilet.metrotoilet.repositories.*
+import org.springframework.stereotype.Service
+
+@Service
+class Service(
+    private val kricHttpAdapter: KricHttpAdapter,
+    private val metroRepository: MetroRepository,
+    private val metroToiletRepository: MetroToiletRepository
+) {
+    fun findAllStation(line_code: String) {
+        return metroRepository.findAll()
+    }
+
+    fun findOneStationToilet(station_code: String) {
+        return metroToiletRepository.findOne()
+    }
+
+    fun syncStations(line_code: String?) {
+        val stations = kricHttpAdapter.requestStation()
+        metroRepository.updateOne(stations)
+    }
+
+    fun syncStationsToilet(line_code: String?, station_code: String?) {
+        val stations = kricHttpAdapter.requestStation()
+
+        for (station in stations) {
+            val toilet = kricHttpAdapter.requestStationToilet(station)
+            metroRepository.updateOne(toilet)
+        }
+    }
+}
