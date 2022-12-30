f10c0d439330b64df1ebb4ad96fb53d7b9246c98
Hwang Gyuyeon <hwgyun@gmail.com>
2022-12-31 06:32:05 +0900

    Modify Cron

--- src/main/kotlin/metrotoilet/metrotoilet/controllers/CronController.kt
+++ src/main/kotlin/metrotoilet/metrotoilet/controllers/CronController.kt
@@ -6,13 +6,13 @@ import org.springframework.scheduling.annotation.Scheduled
 
 @Controller
 class CronController(private val service: Service) {
-    @Scheduled(cron = "* * */15 * *")
+    @Scheduled(cron = "0 0 0/15 * * ?")
     fun syncStations() {
-        return service.syncStations()
+//        return service.syncStations()
     }
 
-    @Scheduled(cron = "* * */15 * *")
+    @Scheduled(cron = "0 0 0/15 * * ?")
     fun syncStationsToilet() {
-        return service.syncStationsToilet()
+//        return service.syncStationsToilet()
     }
 }
