150116f16c43f90dfd117e20d4bd05d08d786fc4
LeeShinYeoung <iqrash12@gmail.com>
2023-01-06 03:28:36 +0900

    Add findTopByLineCode method to repository

--- src/main/kotlin/metrotoilet/metrotoilet/repositories/MetroRepository.kt
+++ src/main/kotlin/metrotoilet/metrotoilet/repositories/MetroRepository.kt
@@ -2,7 +2,14 @@ package metrotoilet.metrotoilet.repositories
 
 import metrotoilet.metrotoilet.domains.*
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface MetroRepository: JpaRepository<Metro, Int>
+interface MetroRepository: JpaRepository<Metro, Int> {
+    fun findTopByLineCode(lineCode: String): Metro? {
+        return findTopByLineCode(lineCode)
+    }
+}
--- src/main/kotlin/metrotoilet/metrotoilet/repositories/MetroToiletRepository.kt
+++ src/main/kotlin/metrotoilet/metrotoilet/repositories/MetroToiletRepository.kt
@@ -1,8 +1,13 @@
 package metrotoilet.metrotoilet.repositories
 
 import metrotoilet.metrotoilet.domains.*
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.*
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface MetroToiletRepository: JpaRepository<MetroToilet, Int>
+interface MetroToiletRepository: JpaRepository<MetroToilet, Int> {
+    fun findTopByLineCode(lineCode: String): MetroToilet? {
+        return findTopByLineCode(lineCode)
+    }
+}
