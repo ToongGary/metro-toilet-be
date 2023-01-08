b067e2834d1c2e9bcb37db98cdd70e7f166222ac
LeeShinYeoung <iqrash12@gmail.com>
2023-01-08 23:46:27 +0900

    Add RestController annotation to CronController

--- src/main/kotlin/metrotoilet/metrotoilet/controllers/CronController.kt
+++ src/main/kotlin/metrotoilet/metrotoilet/controllers/CronController.kt
@@ -4,8 +4,10 @@ import metrotoilet.metrotoilet.services.*
 import org.springframework.stereotype.Controller
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @Controller
+@RestController
 class CronController(private val service: Service) {
     @Scheduled(cron = "0 0 0/15 * * ?")
     @GetMapping("/syncStations")
